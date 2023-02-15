ed4e22ce1f5998326d2b8907bdbbf9c9a8e39eb5
0x <0x@192.168.2.12>
2023-02-15 19:52:35 +0700

    Update meta title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Global Interlink launchpad</title>
+        <title>Sayaka Marketplace</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
       </Head>
       <body>
