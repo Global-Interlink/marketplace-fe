99b586182995944858a0eac1b338ba8cdeda162e
0x <0x@192.168.2.12>
2023-04-24 21:03:09 +0700

    Fix list

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,36 +7,25 @@ import ListCollectionItem from "../src/components/molecules/ListCollectionItem";
 import BaseComponent from "../src/components/organisms/BaseComponent";
 import {
   fetchListCollection,
+  fetchListCollectionLoadmore,
 } from "../src/redux/home/homeSlice";
 import { useAppDispatch, useAppSelector } from "../src/redux/hook";
-import { getUnique } from "../src/utils/localStorage";
-import { Collection } from "../src/api/types";
 
 const Home = () => {
   const dispatch = useAppDispatch();
   const { response, status } = useAppSelector((store) => store.home.homeData);
   const LIMIT = 12;
   const [sort, setSort] = React.useState<"ASC" | "DESC">("DESC");
-  const [currentPage, setCurrentPage] = React.useState(1);
-  const [listCollection, setListCollection] = React.useState<Collection[]>([]);
 
   React.useEffect(() => {
-      dispatch(
-        fetchListCollection({
-          page: currentPage,
-          limit: LIMIT,
-          sort: sort,
-        })
-      );
-  }, [currentPage, dispatch, sort]);
-
-  React.useEffect(() => {
-    if (response?.data) {
-      setListCollection(
-        getUnique([...listCollection, ...response?.data], "id")
-      );
-    }
-  }, [currentPage, response?.data]);
+    dispatch(
+      fetchListCollection({
+        page: 1,
+        limit: LIMIT,
+        sort: sort,
+      })
+    );
+  }, []);
 
   return status === FetchStatus.idle || status === FetchStatus.pending ? (
     <BaseComponent>
@@ -52,7 +41,7 @@ const Home = () => {
         </div>
         {response && response.data && response.data.length > 0 ? (
           <div className="py-4 md:py-6 grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-5">
-            {listCollection.map((i) => {
+            {response.data.map((i) => {
               return i?.creator && <ListCollectionItem key={i.id} data={i} />;
             })}
           </div>
@@ -66,7 +55,13 @@ const Home = () => {
               <button
                 onClick={() => {
                   if (response?.meta.currentPage < response?.meta.totalPages) {
-                    setCurrentPage(currentPage + 1)
+                    dispatch(
+                      fetchListCollectionLoadmore({
+                        page: response?.meta.currentPage + 1,
+                        limit: LIMIT,
+                        sort: sort,
+                      })
+                    );
                   }
                 }}
                 className="bg-white text-primary dark:bg-[#71659C] dark:text-white font-bold rounded-lg border border-[#c2c2c2] w-[189px] h-[49px]"
