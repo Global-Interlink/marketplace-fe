2fb3899e46993e329e710a76e44921940b2f14b6
thangdt <dtthang.5159@gmail.com>
2023-05-05 15:38:19 +0700

    add GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,32 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <title>Sakaya Marketplace</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+        />
+        {/* Google tag (gtag.js) */}
+        <Script
+          strategy="afterInteractive"
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-705HSW11HG"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-705HSW11HG', {
+          page_path: window.location.pathname,
+          });
+        `}
+        </Script>
       </Head>
       <body>
         <Main />
