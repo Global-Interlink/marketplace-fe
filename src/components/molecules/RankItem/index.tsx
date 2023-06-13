d371a0e4de6f721d31258f3b065b4098e0d36264
0x <0x@192.168.2.4>
2023-06-13 21:26:19 +0700

    Update size icon

--- src/components/molecules/RankItem/index.tsx
+++ src/components/molecules/RankItem/index.tsx
@@ -8,12 +8,12 @@ interface Props {
 }
 const RankItem: React.FC<Props> = ({ image, address, percent, tickets }) => {
   return (
-    <div className="bg-gray-50 break-all flex items-center border-2 py-[14px] text-gray-700 px-6 rounded-lg space-x-4 justify-between">
+    <div className="bg-gray-50 break-all flex items-center border-2 py-[14px] text-gray-700 px-4 lg:px-6 rounded-lg space-x-2 md:space-x-4 justify-between">
       <Image
         preview={false}
         width={60}
         height={43}
-        className="min-w-[60px]"
+        className="w-[40px] lg:min-w-[60px]"
         src={image}
         alt={image}
       />
