af788773d86d8af5eea7186c05892b5f8e3aa7aa
hoanganhvn <nguyenhoanganhvn90@gmail.com>
2023-07-07 12:04:20 +0700

    icon social in collection

--- src/components/molecules/SocialView/index.tsx
+++ src/components/molecules/SocialView/index.tsx
@@ -7,6 +7,8 @@ interface Props {
 }
 const SocialView: React.FC<Props> = ({ response }) => {
   const { theme } = useTheme();
+  console.log("response",response);
+  
   return (
     <>
       {response?.website_url && (
@@ -47,6 +49,56 @@ const SocialView: React.FC<Props> = ({ response }) => {
           className="h-[24px] !w-[24px] bg-[#A0A0A0]/50 hover:bg-[#F626D1] rounded !flex justify-center items-center transition-all duration-500"
         >
           <Image src={"/ic-twitter.svg"} width={15} height={15} alt={""} />
+        </a>
+      )}
+          {response?.facebook_url && (
+        <a
+          href={response?.facebook_url}
+          target="_blank"
+          rel="noreferrer"
+          className="h-[24px] !w-[24px] bg-[#A0A0A0]/50 hover:bg-[#F626D1] rounded !flex justify-center items-center transition-all duration-500"
+        >
+          <Image src={"/ic-facebooknew.svg"} width={15} height={15} alt={""} />
+        </a>
+      )}
+          {response?.github_url && (
+        <a
+          href={response?.github_url}
+          target="_blank"
+          rel="noreferrer"
+          className="h-[24px] !w-[24px] bg-[#A0A0A0]/50 hover:bg-[#F626D1] rounded !flex justify-center items-center transition-all duration-500"
+        >
+          <Image src={"/ic-github.svg"} width={15} height={15} alt={""} />
+        </a>
+      )}
+          {response?.instagram_url && (
+        <a
+          href={response?.instagram_url}
+          target="_blank"
+          rel="noreferrer"
+          className="h-[24px] !w-[24px] bg-[#A0A0A0]/50 hover:bg-[#F626D1] rounded !flex justify-center items-center transition-all duration-500"
+        >
+          <Image src={"/ic-instagram.svg"} width={15} height={15} alt={""} />
+        </a>
+      )}
+          {response?.reddit_url && (
+        <a
+          href={response?.reddit_url}
+          target="_blank"
+          rel="noreferrer"
+          className="h-[24px] !w-[24px] bg-[#A0A0A0]/50 hover:bg-[#F626D1] rounded !flex justify-center items-center transition-all duration-500"
+        >
+          <Image src={"/ic-reddit.svg"} width={15} height={15} alt={""} />
+        </a>
+      )}
+          {response?.youtube_url && (
+        <a
+          href={response?.youtube_url}
+          target="_blank"
+          rel="noreferrer"
+          className="h-[24px] !w-[24px] bg-[#A0A0A0]/50 hover:bg-[#F626D1] rounded !flex justify-center items-center transition-all duration-500"
+        >
+          <Image src={"/ic-youtube.svg"} width={15} height={15} alt={""} />
         </a>
       )}
     </>
