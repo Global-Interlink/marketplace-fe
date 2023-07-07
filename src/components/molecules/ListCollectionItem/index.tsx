a3a12d9b5448dbe385cdb9aafe5ae302de520f21
hoanganhvn <nguyenhoanganhvn90@gmail.com>
2023-07-07 10:29:07 +0700

    light mode and  Dark mode Background

--- src/components/molecules/ListCollectionItem/index.tsx
+++ src/components/molecules/ListCollectionItem/index.tsx
@@ -11,9 +11,9 @@ interface Props {
 const ListCollectionItem: React.FC<Props> = ({ data }) => {
   return (
     <Link href={`/collection/${data.id}`}>
-      <div className="flex flex-col w-full shadow-collectionItem dark:shadow-none bg-transparent rounded-[20px] hover:scale-105 transition duration-300 ease-in-out">
+      <div className="flex flex-col w-full shadow-collectionItem dark:shadow-none bg-transparent rounded-[20px] hover:scale-105 transition duration-300 ease-in-out dark:bg-bgLinearCollectionItem">
         <div className="relative">
-          <div className="rounded-t-[20px] dark:bg-bgLinearCollectionItem">
+          <div className="rounded-t-[20px] ">
             <Image
               src={validURL(data?.featuredImage || "/default.jpeg")}
               width={500}
@@ -30,7 +30,7 @@ const ListCollectionItem: React.FC<Props> = ({ data }) => {
             className="rounded-full w-[80px] h-[80px] min-w-[52px] object-cover absolute -bottom-4 right-10 border-4 border-white"
           />
         </div>
-        <div className="group flex p-5 w-full space-x-[14px] dark:bg-bgLinearCollectionItem bg-bgLinearLight  dark:border-none rounded-b-[20px] ">
+        <div className="group flex p-5 w-full space-x-[14px] dark:border-none rounded-b-[20px] ">
           <div className="truncate text-primary dark:text-white">
             <div className=" relative flex items-center space-x-1">
               <p className="truncate">
