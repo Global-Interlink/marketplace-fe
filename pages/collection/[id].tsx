7976d2f2334c23532b7ba53569dcf88707cdd24b
hoanganhvn <nguyenhoanganhvn90@gmail.com>
2023-07-10 10:09:54 +0700

    done 194 ui banner

--- pages/collection/[id].tsx
+++ pages/collection/[id].tsx
@@ -130,13 +130,13 @@ const Collection = () => {
               className="flex w-full aspect-[1305/228] rounded-[20px] object-cover mt-5"
               alt="banner"
             />
-            <div className="flex w-[80%] mx-[10%] bg-white dark:bg-[#1F0844] rounded-[10px] -mt-6 sm:-mt-14 z-[9999] relative">
+            <div className="flex w-[80%] mx-[10%] bg-white dark:bg-[#1F0844] rounded-[10px] -mt-0 sm:-mt-14 z-[9999] relative">
               <Image
                 alt="logo-lp"
                 src={collectionData?.logo}
                 width={160}
                 height={160}
-                className=" w-[60px] h-[60px] sm:w-[160px] sm:h-[160px] aspect-[1/1] object-cover rounded-[10px] m-[12px] sm:m-[24px] items-center"
+                className=" w-[60px] h-[60px] sm:w-[160px] sm:h-[160px] aspect-[1/1] object-cover rounded-[10px] m-[14px] sm:m-[24px] items-center"
               />
               <div className="w-full my-auto">
                 <p
@@ -180,7 +180,7 @@ const Collection = () => {
                     {showMore ? <BsChevronUp /> : <BsChevronDown />}
                   </button>
                 </div>
-                <div className="flex items-center space-x-3">
+                <div className="flex items-center space-x-3 mb-2 sm:mb-0">
                   <SocialView response={collectionData} />
                 </div>
               </div>
