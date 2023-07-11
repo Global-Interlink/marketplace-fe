a963400d8d762144b5431eba4c7edf1315d97119
Duke Pham <strawberrycsgo@gmail.com>
2023-07-11 17:15:47 +0700

    fix padding collection detail

--- pages/collection/[id].tsx
+++ pages/collection/[id].tsx
@@ -138,7 +138,7 @@ const Collection = () => {
                 height={160}
                 className=" w-[60px] h-[60px] sm:w-[160px] sm:h-[160px] aspect-[1/1] object-cover rounded-[10px] m-[14px] sm:m-[24px] items-center"
               />
-              <div className="w-full mt-3">
+              <div className="w-full my-3">
                 <p
                   className="text-xl md:text-[24px] external leading-8 font-semibold text-black dark:text-white font-display break-all mr-[24px]"
                   title={collectionData?.name}
